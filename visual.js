e4152df90882c66a2a7cb2cadb5cfec22d222a30
Christopher Markieta <markietachristopher@gmail.com>
2015-10-09 00:32:58 -0400

    Added visual mode

---
+++ visual.js
@@ -0,0 +1,24 @@
+Enter Visaul Mode:
+    CTRL+v      Visual block mode
+    Shift+v     Visual line mode
+
+if(true)
+{
+i = ">";
+n = ">";
+d = ">";
+e = ">";
+n = ">";
+t = ">";
+}
+else
+{
+            r = "3<";
+            e = "3<";
+            i = "3<";
+            n = "3<";
+            d = "3<";
+            e = "3<";
+            n = "3<";
+            t = "3<";
+}
